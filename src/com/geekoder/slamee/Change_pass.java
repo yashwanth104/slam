6d4b340a79ee38d577bed5bc5962f3274bc02d01
DESKTOP-RN2K5O7\Yashwanth <gupta.yashwanth@gmail.com>
2018-03-16 15:58:53 +0100

    for demo video

--- src/com/geekoder/slamee/Change_pass.java
+++ src/com/geekoder/slamee/Change_pass.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 //testing for auto build on push
+//testing changes to commit for video demoo
 public class Change_pass extends Activity {
 TextView chng_hdr,chng_old_passtxt;
 EditText chng_old_pass;
